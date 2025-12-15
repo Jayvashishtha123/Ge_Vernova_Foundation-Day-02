c47c825c85510f6abf122262ef92a388c2d8c5b9
Jay Vardhan Vashishtha <jayvardhanvashishtha@Jays-MacBook-Air.local>
2025-12-16 02:45:00 +0530

    Welcome message displayed on master branch

---
+++ src/Employee_wage/Employe_wage_part01.java
@@ -0,0 +1,7 @@
+package Employee_wage;
+
+public class Employe_wage_part01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
